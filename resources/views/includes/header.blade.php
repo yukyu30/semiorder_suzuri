3d04ea51e607cba7c8163b3a1bd8576e0c11f312
yukyu30 <fukanoyugo+dev@gmail.com>
2020-11-30 15:20:28 +0900

    最初のコミット

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <a class="navbar-item" href="">
+            <h1 class="is-size-4 has-text-weight-semibold">BECCHU</h1>
+        </a>
+        
+        <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>
+    </div>
+        
+    <div class="navbar-menu">
+        <div class="navbar-start">
+            <a class="navbar-item" href="#profile">
+                <p class="is-size-5">About</p>
+            </a>
+        
+            <a class="navbar-item" href="#skills">
+                <p class="is-size-5">OnlineStore</p>
+            </a>
+
+            <a class="navbar-item" href="#links">
+                <p class="is-size-5">links</p>
+            </a>
+
+            <a class="navbar-item" href="#contact">
+                <p class="is-size-5">contact</p>
+            </a>
+        </div>
+    </div>
+</nav>

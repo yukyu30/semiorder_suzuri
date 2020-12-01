d7aa43f4aa4bce59856c944f1ad0758647ac1e4f
yukyu30 <fukanoyugo+dev@gmail.com>
2020-12-01 22:51:31 +0900

    最低限の動作をした

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,13 +1,19 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="stylesheet"href="{{ asset('css/app.css') }}" >
+<link rel="stylesheet" href="{{ asset('css/app.css') }}" >
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet">
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/themes/monolith.min.css"/>
+<script type="text/javascript" src="https://code.jquery.com/jquery.js"></script>
+<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+
 <script>
     $(document).ready(function() {  
-        $(".navbar-burger").click(function() {
-            $(".navbar-burger").toggleClass("is-active");
-            $(".navbar-menu").toggleClass("is-active");
-        });
+    $(".navbar-burger").click(function() {
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").toggleClass("is-active");
     });
+});
 </script>
+
+
+
